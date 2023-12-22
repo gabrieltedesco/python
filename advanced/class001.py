a8e46ceae70a690d223e1990f1bcdad9ef09100f
Gabriel Tedesco Gotz <136640748+gabrieltedesco@users.noreply.github.com>
2023-12-22 20:38:12 -0300

    Create class001.py

---
+++ advanced/class001.py
@@ -0,0 +1,35 @@
+#LISTS
+my_list = ["banana", "cherry"]
+my_list2 = list(my_list)
+my_list2.append("apple")
+my_list.clear()
+list_comp = [i**2 for i in range(1, 5)]
+
+#TUPLES
+my_tuple = ("banana", "cherry")
+
+#DICTIONARIES
+my_dict = {"fruit1": "banana", "fruit2":"cherry"}
+my_dict["fruit3"] = "apple"
+del my_dict["fruit3"]
+for key, value in my_dict.items():
+    print(key, value)
+
+#SETS
+#STRINGS
+#COLLECTIONS
+#ITERTOOLS
+#LAMBDA FUNCTIONS
+#EXCEPTIONS AND ERRORS
+#LOGGING
+#JSON
+#RANDOM NUMBERS
+#DECORATORS
+#GENERATORS
+#THREADING
+#MULTITHREADING
+#MULTUPROCESSING
+#FUNCTION ARGUMENTS
+#ASTERISK
+#SHALLOW
+#CONTEXT MANAGERS
